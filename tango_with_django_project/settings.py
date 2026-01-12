1cccf51d5906be2838a78d59e803712c45252285
Zak Grivell <zak@grivell.uk>
2026-01-12 18:36:07 +0000

    ch 4.3 - media setup

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -19,6 +19,8 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
 
 STATIC_DIR = os.path.join(BASE_DIR, "static")
 
+MEDIA_DIR = os.path.join(BASE_DIR, "media")
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -67,6 +69,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "django.template.context_processors.media",
             ],
         },
     },
@@ -127,3 +130,6 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_URL = "/static/"
+
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = "/media/"
