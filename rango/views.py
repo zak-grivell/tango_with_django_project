f90750ff306a4a39dea7e8f4cc9c1f54a631699c
Zak Grivell <zak@grivell.uk>
2026-01-12 18:06:20 +0000

    add slash to pass test

--- rango/views.py
+++ rango/views.py
@@ -9,4 +9,6 @@ def index(request):
 
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. <a href='/rango'>Index</a>")
+    return HttpResponse(
+        "Rango says here is the about page. <a href='/rango/'>Index</a>"
+    )
