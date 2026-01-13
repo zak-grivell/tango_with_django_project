e1c9b212c083b22b66e7de98f0c67064862c1c73
Zak Grivell <zak@grivell.uk>
2026-01-13 18:02:45 +0000

    9.9 - logout

--- rango/urls.py
+++ rango/urls.py
@@ -17,4 +17,5 @@ urlpatterns = [
     path("register/", views.register, name="register"),
     path("login/", views.user_login, name="login"),
     path("restricted/", views.restricted, name="restricted"),
+    path("logout/", views.user_logout, name="logout"),
 ]
--- rango/views.py
+++ rango/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
@@ -147,3 +147,9 @@ def user_login(request):
 @login_required
 def restricted(request):
     return HttpResponse("Since you're logged in, you can see this text!")
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect(reverse("rango:index"))
