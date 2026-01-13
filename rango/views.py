4dbba2482579dba80cb8862f26b046c8421e6d7a
Zak Grivell <zak@grivell.uk>
2026-01-13 11:27:00 +0000

    ch 8.1 - 8.4

--- rango/views.py
+++ rango/views.py
@@ -46,7 +46,7 @@ def add_category(request):
         form = CategoryForm(request.POST)
         if form.is_valid():
             form.save(commit=True)
-            return redirect("/rango/")
+            return redirect(reverse("rango:index"))
         else:
             print(form.errors)
 
@@ -60,7 +60,7 @@ def add_page(request, category_name_slug):
         category = None
 
     if category is None:
-        return redirect("/rango/")
+        return redirect(reverse("rango:index"))
     form = PageForm()
 
     if request.method == "POST":
