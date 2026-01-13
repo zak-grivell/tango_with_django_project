d77b1eb652bcb6acd59e159b95ada45a130769b0
Zak Grivell <zak@grivell.uk>
2026-01-13 17:30:19 +0000

    ch 9.1 - 9.7

--- rango/urls.py
+++ rango/urls.py
@@ -14,4 +14,5 @@ urlpatterns = [
     path(
         "category/<slug:category_name_slug>/add_page/", views.add_page, name="add_page"
     ),
+    path("register/", views.register, name="register"),
 ]
