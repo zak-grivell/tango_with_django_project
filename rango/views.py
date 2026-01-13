6a0f1d298bc911df9af3cf45afb636b4b16498e6
Zak Grivell <zak@grivell.uk>
2026-01-13 17:36:25 +0000

    ch 9.7

--- rango/views.py
+++ rango/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.urls import reverse
 
@@ -120,3 +122,22 @@ def register(request):
             "registered": registered,
         },
     )
+
+
+def user_login(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return redirect(reverse("rango:index"))
+            else:
+                return HttpResponse("Your Rango account is disabled.")
+        else:
+            print(f"Invalid login details: {username}, {password}")
+            return HttpResponse("Invalid login details supplied.")
+
+    return render(request, "rango/login.html")
